5942c94bede9f921fe72a8d5892ecc514d7c256d
Anyalee610 <anayalambright610@gmail.com>
2023-01-18 15:15:44 -0500

    all done

--- exercises.test.js
+++ exercises.test.js
@@ -12,7 +12,9 @@ test('Question 1 isRealPalindrome', () => {
 test('Question 2 runningTotal', () => {
   expect(exercises.runningTotal([1,2,3])).toEqual([1,3,6]);
   expect(exercises.runningTotal([])).toEqual([]);
+  expect(exercises.runningTotal([14, 11, 7, 15, 20])).toEqual([14, 25, 32, 47, 67]);
   expect(exercises.runningTotal([3])).toEqual([3]); 
+  
 })
 
 test('Question 3 swap', () => {
@@ -27,6 +29,7 @@ test('Question 4 wordSizes', () => {
   expect(exercises.wordSizes('123')).toEqual({'3': 1});
   expect(exercises.wordSizes('')).toEqual({});
   expect(exercises.wordSizes('Hey diddle diddle, the cat and the fiddle!')).toEqual({ "3": 5, "6": 1, "7": 2 }); 
+  expect(exercises.wordSizes("What's up Ana?")).toEqual({ "2": 1, "4": 1, "6": 1 });
 })
 
 test('Question 5 union', () => {
@@ -41,7 +44,6 @@ test('Question 6 firstRecurring', () => {
   expect(exercises.firstRecurring('123')).toEqual('');
   expect(exercises.firstRecurring('abaa')).toEqual('a');
   expect(exercises.firstRecurring('abba')).toEqual('b');
-  
 })
 
 test('Question 7 showMultiplicativeAverage', () => {
